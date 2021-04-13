543f5a9b0bf20f93d7179233c3bd3d3f89e04ab0
Hasan2510 <hasan251096@outlook.com>
2021-04-13 16:28:35 +0100

    CH-19 CH-71 CH-70 CH-72 CH-20 CH-65 CH-66 CH-67 CH-21 CH-49 CH-55 Ch-56 CH-22 CH-100 CH-98 CH-101 CH-23 CH-85 CH-87 CH-90 #fixed playlists

--- FinalProjectWesbite/javascript/Playlists.js
+++ FinalProjectWesbite/javascript/Playlists.js
@@ -1,6 +1,5 @@
 
-function fetchPlaylists(){
-  console.log("fetching");
+function fetchplay(){
   fetch("http://localhost:8090/playlists/read") // 1
     .then(response => {
         if (response.status !== 200) {  //  2
@@ -9,10 +8,58 @@ function fetchPlaylists(){
             return;
         }
         response.json() // 3
-        .then(data => console.info(data))
-        //.then(data  => createPlaylistTable(data))// 4
+        //.then(data => console.info(data))
+        .then(data  => createPlaylistTable(data))// 4
     }).catch((err)=> console.error(`${err}`)); // 5
-}
+  }
+
+  function createPlaylistTable(data){
+
+    console.log(data);
+    deleteTable();
+  
+    let table = document.getElementById("PlayResultSet");
+    let row =  document.createElement('tr');
+    let idHeader = document.createElement('th');
+    let nameHeader = document.createElement('th');
+    let descriptionHeader = document.createElement('th');
+    let artworkHeader = document.createElement('th');
+    idHeader.innerHTML= "Play-ID";
+    nameHeader.innerHTML = "Play Name";
+    descriptionHeader.innerHTML = "Play Description";
+    artworkHeader.innerHTML = "Play Artwork";
+    row.appendChild(idHeader);
+    row.appendChild(nameHeader);
+    row.appendChild(descriptionHeader);
+    row.appendChild(artworkHeader);
+    table.appendChild(row);
+  
+    for(let i = 0; i<data.length; i++){
+     console.log(data[i].name); 
+     let row = document.createElement('tr');
+     let cell1 = document.createElement('td')
+     let cell2 = document.createElement('td');
+     let cell3 = document.createElement('td');
+     let cell4 = document.createElement('td');
+     cell1.innerHTML = data[i].id;
+     cell2.innerHTML = data[i].name;
+     cell3.innerHTML = data[i].description;
+     cell4.innerHTML = data[i].artwork;
+     row.appendChild(cell1);
+     row.appendChild(cell2);
+     row.appendChild(cell3);
+     row.appendChild(cell4);
+     table.appendChild(row); 
+   }
+  }
+
+  function deleteTable(){
+    let i = document.getElementById("PlayResultSet").rows.length-1
+    for (i; i>=0; i--){
+      document.getElementById('PlayResultSet').deleteRow(i);
+      console.log("deleted row");
+    }
+  }
 
 function createPlaylist() {
     fetch("http://localhost:8090/playlists/create", {
@@ -21,13 +68,14 @@ function createPlaylist() {
           "Content-type": "application/json"
       },
         body: JSON.stringify({
-           name: document.querySelector("#createPlaylistname"),
-           description: document.querySelector("#createCover").value
+           name: document.querySelector("#createPlaylistsName").value,
+           description: document.querySelector("#createPlaylistsDescription").value,
+           artwork: document.querySelector("#createPlaylistsArtwork").value
 
          })
         })
         .then(res => {
-          if(res.status!=200){
+          if(res.status!=201){
               console.error(res)
           }  
             res.json()})
@@ -43,15 +91,16 @@ function createPlaylist() {
     
 
      function updatePlaylist(){
-    let Pid= parseInt(document.querySelector("#updatePlaylistId").value)
-    fetch("http://localhost:8080/Playlist/"+Pid, {
+    let Pid= parseInt(document.querySelector("#updatePlaylistsId").value)
+    fetch("http://localhost:8090/playlists/update/"+Pid, {
        method: 'put',
         headers: {
           "Content-type": "application/json"
       },
         body: JSON.stringify({
-            name: document.querySelector("#createPlaylistname"),
-            description: document.querySelector("#createCover").value
+            name: document.querySelector("#updatePlaylistsName").value,
+            description: document.querySelector("#updatePlaylistsDescription").value,
+            artwork: document.querySelector("#updatePlaylistsArtwork").value
             
            
          })
@@ -72,8 +121,8 @@ function createPlaylist() {
 
 
     function delete_Playlist() {
-        let pid= parseInt(document.querySelector("#DA_id").value)
-         fetch("http://localhost:8080/Playlist/"+pid, {//2
+        let pid= parseInt(document.querySelector("#delIdCheck").value)
+         fetch("http://localhost:8090/playlists/delete/"+pid, {//2
              method: 'delete',//3
            })
            .then((data) => {
